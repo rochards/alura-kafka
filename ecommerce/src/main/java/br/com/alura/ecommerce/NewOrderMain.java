7208488eb7c018ea9686431748ca63b65b3896ad
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-05-03 09:16:39 -0300

    Add produtor de mensagens
    Cria um produtor de mensagens para o Kafka

--- ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,7 +1,37 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
 public class NewOrderMain {
-    public static void main(String[] args) {
-        System.out.println("Olá");
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // esses properties poderia ser na verdade um arquivo
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "1234134, 1245, 1234.00";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> { // send é assíncrono, por isso damos um get
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso " + data.topic() + "::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        }).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+
+        // onde está o Kafka?
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        // de que tipo é a chave?
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // de que tipo é a mensagem?
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
 }

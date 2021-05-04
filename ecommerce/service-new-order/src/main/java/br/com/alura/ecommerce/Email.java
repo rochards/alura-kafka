12a750071f4fe476e3fffc3bc9e6dd4ae6c9e4ae
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-05-03 21:20:58 -0300

    Refatoracoes
    - Remove dependencias desnecessarias dos arquivos pom.xml;
    - Move classes de modulos;
    - Faz copias de classes do dominio para que essas nao fiquem
      compartilhadas entre modulos.

---
+++ ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/Email.java
@@ -0,0 +1,12 @@
+package br.com.alura.ecommerce;
+
+public class Email {
+
+    private String subject;
+    private String body;
+
+    public Email(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+}
